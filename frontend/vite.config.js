6072510bd8604c9f1fd11a5f127ba8a99f8070ff
siddhant4357 <siddhantsankesara@gmail.com>
2025-06-21 19:25:41 +0530

    check this

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom']
-        }
-      }
-    }
-  },
   server: {
-    host: '0.0.0.0',
     port: 5173,
     proxy: {
       '/api': {
