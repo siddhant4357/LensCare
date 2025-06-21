d20e8711430d143cc37a1d638b02657fe2adddf6
siddhant4357 <siddhantsankesara@gmail.com>
2025-06-21 19:22:27 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
+  },
   server: {
+    host: '0.0.0.0',
     port: 5173,
     proxy: {
       '/api': {
